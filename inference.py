28055231c68f64b0aa18463032ec01af8a1502ea
Raja Raju <pgrajaraju@hotmail.com>
2023-06-23 00:33:02 +0530

    loading state dict

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
+import torch
 from dinov2.models.vision_transformer import vit_base
 
 model = vit_base()
 
-for name, param in model.named_parameters():
-    print(name, param.shape)
+state_dict = torch.hub.load_state_dict_from_url("https://dl.fbaipublicfiles.com/dinov2/dinov2_vitb14/dinov2_vitb14_pretrain.pth")
+model.load_state_dict(state_dict)
